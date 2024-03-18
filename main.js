5bd3e93b260c40b75f34e52f2fa0fa17b7189a99
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-03-18 22:44:05 +0500

    js 0.1

---
+++ main.js
@@ -0,0 +1,59 @@
+// Chapter 14-16
+
+// Q1
+// var studnetName = []
+
+
+// Q2
+// var studnetName = []
+
+
+// Q3
+// var humanName = ['name1','name2','name3']
+
+// Q4
+// var num = [1,2,3,3]
+
+
+// Q5
+// var condition [ture,false,ture,ture]
+
+
+// Q6
+// var mix = [true, 'name1', 234]
+
+
+//Q7
+// var qual = [' SSC', ' HSC', ' BCS', ' BS', ' BCOM', ' MS', ' M.Phil', ' PhSD']
+// var num = 1
+// document.write('Quatification <br>',
+//     num++,qual[0],' <br>',
+//     num++,qual[1],' <br>',
+//     num++,qual[2],' <br>',
+//     num++,qual[3],' <br>',
+//     num++,qual[4],' <br>',
+//     num++,qual[5],' <br>',
+//     num++,qual[7],' <br>',
+// )
+
+
+// Q8
+// var studentName1 = prompt('Enter your name')
+// var studentMarks1 = +prompt('Enter your marks')
+// var percen1 =  studentMarks1/500 *100
+// document.write(
+//     `score of ${studentName1} is ${studentMarks1}. Percentage: ${percen1}% <br>`
+// )
+// var studentName2 = prompt('Enter your name')
+// var studentMarks2 = +prompt('Enter your marks')
+// var percen2 = studentMarks2 /500*100
+// document.write(
+//     `score of ${studentName2} is ${studentMarks2}. Percentage: ${percen2}% <br>`
+// )
+// var studentName3 = prompt('Enter your name')
+// var studentMarks3 = +prompt('Enter your marks')
+// var percen3 = studentMarks3/500 *100
+// document.write(
+//     `score of ${studentName3} is ${studentMarks3}. Percentage: ${percen3}% <br>`
+// )
+
